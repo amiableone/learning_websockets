1d56feb49e2cbab8218fa757c563c70f197d204d
Albert Safin <albertsafinf@gmail.com>
2023-06-08 11:10:35 +0200

    added getWebSocketServer() and modified initialization in main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const board = document.querySelector(".board");
   createBoard(board);
   // Open the Websocket connection and register event handlers
-  const websocket = new WebSocket("ws://localhost:8001");
+  const websocket = new WebSocket(getWebSocketServer());
   initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
@@ -80,4 +80,14 @@ function initGame(websocket) {
     }
     websocket.send(JSON.stringify(event));
   });
+}
+
+function getWebSocketServer() {
+  if (window.location.host === "aaugustin.github.io") {
+    return "wss://websockets-tutorial.herokuapp.com/";
+  } else if (window.location.host === "localhost:8000") {
+    return "ws://localhost:8001/";
+  } else {
+    throw new Error(`Unsupported host: ${window.location.host}`);
+  }
 }
